e5acf7116c253f353521949bc8b454ef35881a66
Tarocco <bun@taroc.co>
2019-05-25 18:39:40 -0700

    temp commit

--- client/dist/js/index.js
+++ client/dist/js/index.js
@@ -7,7 +7,8 @@ let nav_actors_map = nav_actors.reduce(function (map, obj) {
     let key = obj.getAttribute('href').substr(1);
     if (key == '') // Special case
         key = 'home';
-    map[key] = obj;
+    let id = key + '-section';
+    map[id] = obj;
     return map;
 }, {});
 
@@ -31,7 +32,6 @@ function show_section(section) {
             actor.classList.add('selected');
         else
             actor.classList.remove('selected');
-        console.log(actor)
     }
 }
 
@@ -42,19 +42,24 @@ for (let section_id of section_ids) {
 }
 
 // Adapted from https://alligator.io/js/lazy-loading-scripts/
-function load_script(key, url) {
+async function load_script(key, url) {
     let id = `load-script--${key}`;
     let is_loaded = document.getElementById(id);
     if(is_loaded)
         return null;
-    let script = document.createElement('script');
+    const script = document.createElement('script');
     script.src = url;
     script.id = id;
-    document.body.appendChild(script);
-    return script;
+    return new Promise((resolve, reject) => {
+        function handler() {
+            resolve(true);
+        }
+        script.addEventListener('load', handler);
+        document.getElementsByTagName('head')[0].appendChild(script);
+    });
 }
 
-function load_css(key, url)
+async function load_css(key, url)
 {
     let id = `load-css--${key}`;
     let is_loaded = document.getElementById(id);
@@ -64,16 +69,41 @@ function load_css(key, url)
     link.href = url;
     link.type = 'text/css';
     link.rel = 'stylesheet';
-    document.getElementsByTagName('head')[0].appendChild(link);
+    return new Promise((resolve, reject) => {
+        function handler() {
+            resolve(true);
+        }
+        link.addEventListener('load', handler);
+        document.getElementsByTagName('head')[0].appendChild(link);
+    });
 }
 
-function load_media_player() {
-    // TODO
-    //load_script('jquery', 'js/jquery.js');
+async function load_media_player() {
+    await load_css('wptpa_style', 'css/wptpa_style.css');
+    await load_script('jquery', 'js/jquery.js');
+    await load_script('jquery-ui', 'js/jquery-ui.js');
+    await load_script('snap-svg', 'js/snap.svg.js');
+    await load_script('wptpa', 'js/wptpa_script.js');
+    await load_script('playlists', 'js/playlists.js');
+    let player = $('section#media-section #media-player').tPlayer(
+    {
+        playlist: Playlists.Tracks,
+        playerBG: "#FFF",
+        playerTextCLR: "#F61",
+        buttonCLR: "#F61",
+        buttonActiveCLR: "#F61",
+        seekBarCLR: "#F61",
+        progressBarCLR: "#FFF",
+        timeCLR: "#FFF",
+        playlistBG: "#FFF",
+        playlistTextCLR: "#F61",
+        playlistCurBG: "#F61",
+        playlistTextCurCLR: "#FFF",
+    });
 }
 
 function unload_media_player() {
-    // TODO
+    $('section#media-section #media-player').empty();
 }
 
 function handle_show_media_section(e) {
@@ -85,10 +115,11 @@ function handle_hide_media_section(e) {
 }
 
 let current_section_id = window.location.hash.substring(1);
-let current_section = nav_sections_map[current_section_id] || document.querySelector('section#home');
-let media_section = document.querySelector('section#media');
-media_section.addEventListener('show', handle_show_media_section);
+let current_section = nav_sections_map[current_section_id] || document.querySelector('section#home-section');
+let media_section = document.querySelector('section#media-section');
 show_section(current_section);
+media_section.addEventListener('show', handle_show_media_section);
+media_section.addEventListener('hide', handle_hide_media_section);
 
 let tarocco = Tarocco();
 let cursor_follower = document.querySelector('#cursor-0');
---
+++ client/dist/js/playlists.js
@@ -0,0 +1,11 @@
+Playlists = {
+    Tracks: [
+        {
+            "type": "audio",
+            "artist": "Tarocco",
+            "title": "Star Angel",
+            "audio": "static/media/tracks/Tarocco - Star Angel.mp3",
+            "cover": ""
+        }
+    ]
+};
