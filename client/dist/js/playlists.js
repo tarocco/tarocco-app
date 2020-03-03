ff8bb64d0edb7064ddb89adf02022eb02ff794e2
Tarocco <bun@taroc.co>
2020-03-03 15:02:17 -0800

    download links for media

--- client/dist/js/playlists.js
+++ client/dist/js/playlists.js
@@ -5,6 +5,7 @@ Playlists = {
             "artist": "Tarocco",
             "title": "Electronic Soundtrack Ep. 2",
             "audio": "media/mixes/Electronic Soundtrack 2.mp3",
+            "dwn": "media/mixes/Electronic Soundtrack 2.mp3",
             "cover": "media/mixes/Electronic Soundtrack Ep. 2.jpg"
         },
         {
@@ -12,6 +13,7 @@ Playlists = {
             "artist": "Tarocco",
             "title": "Electronic Soundtrack Ep. 1",
             "audio": "media/mixes/Electronic Soundtrack 1.mp3",
+            "dwn": "media/mixes/Electronic Soundtrack 1.mp3",
             "cover": "media/mixes/Electronic Soundtrack Ep. 1.jpg"
         },
         {
@@ -19,6 +21,7 @@ Playlists = {
             "artist": "Tarocco",
             "title": "waveset2",
             "audio": "media/mixes/Tarocco - waveset2.mp3",
+            "dwn": "media/mixes/Tarocco - waveset2.mp3",
             "cover": "media/mixes/waveset2.jpg"
         },
         {
@@ -26,6 +29,7 @@ Playlists = {
             "artist": "Tarocco",
             "title": "waveset1",
             "audio": "media/mixes/Tarocco - waveset1.mp3",
+            "dwn": "media/mixes/Tarocco - waveset1.mp3",
             "cover": "media/mixes/waveset1.jpg"
         },
         {
@@ -33,6 +37,7 @@ Playlists = {
             "artist": "Tarocco",
             "title": "Star Angel",
             "audio": "media/tracks/Tarocco - Star Angel.mp3",
+            "dwn": "media/tracks/Tarocco - Star Angel.mp3",
             "cover": "media/tracks/Tarocco - Star Angel - 500px.jpg"
         }
     ]
